4e49f07eb8d7706fcc50e79e15a62b79462e2e73
proy <proy@comitfs.com>
2020-05-19 20:49:18 +0100

    OF-2027 Enable PEP service for local users as well as registered users

--- xmppserver/src/main/java/org/jivesoftware/openfire/pep/PEPServiceManager.java
+++ xmppserver/src/main/java/org/jivesoftware/openfire/pep/PEPServiceManager.java
@@ -21,6 +21,7 @@ import org.jivesoftware.openfire.user.UserManager;
 import org.jivesoftware.util.CacheableOptional;
 import org.jivesoftware.util.cache.Cache;
 import org.jivesoftware.util.cache.CacheFactory;
+import org.jivesoftware.util.JiveGlobals;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xmpp.packet.IQ;
@@ -189,8 +190,15 @@ public class PEPServiceManager {
     public PEPService create(JID owner) {
         // Return an error if the packet is from an anonymous, unregistered user
         // or remote user
-        if (!XMPPServer.getInstance().isLocal(owner)
-                || !UserManager.getInstance().isRegisteredUser(owner.getNode())) {
+
+    	boolean isRegistered=false;
+    	if (JiveGlobals.getBooleanProperty("xmpp.pubsub.create.localuser", false)) {
+    		isRegistered = true;
+    	} else {
+    		isRegistered = UserManager.getInstance().isRegisteredUser(owner.getNode());
+    	}
+
+        if (!XMPPServer.getInstance().isLocal(owner) || !isRegistered) {
             throw new IllegalArgumentException(
                     "Request must be initiated by a local, registered user, but is not: "
                             + owner);
